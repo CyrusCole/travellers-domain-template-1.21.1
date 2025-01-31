43b3d871c645142dc13ef7bf525fbdaa106e315e
CyrusCole <cole.keaganhill@gmail.com>
2025-01-31 09:08:11 +0200

    Initial commit

---
+++ src/main/java/com/cyruscole/travellersdomain/TravellersDomainDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cyruscole.travellersdomain;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TravellersDomainDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
